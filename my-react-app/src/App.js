79b3d83b9760f9587ac8b45d95004ee313099cf2
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-10-13 17:39:07 -0300

    finalizada

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ApiPokemon from './Components/ApiPokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiPokemon/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Components/ApiPokemon.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import axios from 'axios';
+
+
+export const ApiPokemon = (props) => {
+
+  const [pokemones, setPokemones]= useState ([])
+
+  const callApi = async () => {
+   const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=870&offset=0")
+   const lista = response.data.results;
+   setPokemones(lista)
+  }
+
+
+  return (
+    <div>
+      <button onClick={callApi} className="btn btn-primary mt-4">Buscar Pokemon</button>
+
+      {
+        pokemones.map((item, index) => {
+          return <p key={index}>{index} {item.name}</p>
+        })
+      }
+    </div>
+  );
+}
+ 
+export default ApiPokemon;
